ed7f1a851cb64a5628a73ba246f9edbf45e2e393
uncluze <uncluze@yandex.ru>
2022-10-29 18:20:16 +0300

    Initial Commit

---
+++ PaletteApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PaletteApp
+//
+//  Created by uncluze on 29.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
